0a7b14f65b5679f25a7efeb8c100ab5d3169d91c
Michael McGraw <info@mcg73.com>
2014-03-25 14:28:25 -0400

    McGraw's 'Dark Wiz Locator' added

--- wiz-snippets.js
+++ wiz-snippets.js
@@ -2,22 +2,22 @@
 
 //1. define a Wiz Class
 function Wiz(name, level) {
-  this.name = name;
-  this.level = level;
-  this.peeDraw = "";//Nic has added this to not have clutter and because he doesn't quite understand js inheritance crap
+    this.name = name;
+    this.level = level;
+    this.peeDraw = ""; //Nic has added this to not have clutter and because he doesn't quite understand js inheritance crap
 }
 
 function compareWizzes(a, b) {
-  if (a.level < b.level)
-    return -1;
-  if (a.level > b.level)
-    return 1;
-  return 0;
+    if (a.level < b.level)
+        return -1;
+    if (a.level > b.level)
+        return 1;
+    return 0;
 }
 
 
 function sort_wiz_levels(wizArray) {
-  return wizArray.sort(compareWizzes);
+    return wizArray.sort(compareWizzes);
 }
 
 // create an array of Wiz's
@@ -36,13 +36,13 @@ console.dir(sortedWizards);
 
 //draw quick/basic ascii art of peeing on wizard
 function drawWizLvl(wiz) {
-  var peePipe = "",
-    peePipeStream = wiz.stream;
+    var peePipe = "",
+        peePipeStream = wiz.stream;
 
-  for (var i = 0; i < peePipeStream; i++) {
-    peePipe += "          |\n";
-  }
-  return wiz.peeDraw = "8---> ---\n" + "         \\" + "\n" + peePipe + "         '-'---\n" + "         | o o |\n" + "         |  >  |\n" + "         |  o  |\n" + "          ----- \n";
+    for (var i = 0; i < peePipeStream; i++) {
+        peePipe += "          |\n";
+    }
+    return wiz.peeDraw = "8---> ---\n" + "         \\" + "\n" + peePipe + "         '-'---\n" + "         | o o |\n" + "         |  >  |\n" + "         |  o  |\n" + "          ----- \n";
 }
 
 var wiz = new Wiz('Mike', 3);
@@ -50,3 +50,18 @@ var draw = drawWizLvl(wiz);
 console.log(draw);
 
 //End Nic's Snippet
+
+//McGraw's Dark Wiz Locator
+
+function darkWizz(wizArray) {
+    var itsLong = wizArray.length;
+    if (itsLong > 0) {
+        for (w = 0; w < itsLong; w++) {
+            if (wizArray[w] == 'Mike') {
+                wizArray[w] = 'Mike the Penis Wizard';
+            }
+        }
+    } else alert("All Hail Mike, The Dark Penis Wizard");
+}
+
+//End McGraw's Dark Wiz Locator
